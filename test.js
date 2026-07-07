c04505171bead8c553733e6fa138c409d82ad674
Talal Azhar <talalazhar5656@gmail.com>
2026-07-07 19:44:18 +0500

    Console.table()

--- test.js
+++ test.js
@@ -1,6 +1,10 @@
-console.log("Talal")
+// console.log("Talal")
 var talal = "Talal"
 console.log(talal)
 console.log(talal+" is 21 year old")
 talal = "Talal Azhar"
 console.log(talal)
+let name = "Talal"
+let age = 21
+let grade = "A"
+console.table([name,age,grade])
