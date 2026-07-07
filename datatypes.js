69967bd0814c033feeefe84b79cce16fb5cf8501
Talal Azhar <talalazhar5656@gmail.com>
2026-07-07 20:24:32 +0500

    Data Type

---
+++ datatypes.js
@@ -0,0 +1,13 @@
+"use strict"; // Treat all code as newer js version
+
+console.log(3 +  
+    3) //readibilty should be high
+
+let name = "Talal" // string
+// let name = 'Talal' // string
+let age = 21 //number ....2 power 53    
+let isLoggedIn = true // boolean
+let temp = null // stand alone value (empty)
+let number = undefined //no value 
+let unique = Symbol
+console.log(typeof age)
