570c78bf7e7e8f12ebc237647bbdcec11130af9a
Talal Azhar <talalazhar5656@gmail.com>
2026-07-08 17:26:12 +0500

    conditions

---
+++ conditions.js
@@ -0,0 +1,5 @@
+console.log("2">2)
+console.log("02"==2)//convert 02 into number
+console.log("02"===2) //it also check datatype
+console.log(null==0)
+console.log(null>=0)//convert null into 0 then compare
