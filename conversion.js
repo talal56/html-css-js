a0ed311f711da8f88b2c060de2ed887e69d337db
Talal Azhar <talalazhar5656@gmail.com>
2026-07-08 16:44:52 +0500

    bool convertion

--- conversion.js
+++ conversion.js
@@ -1,13 +1,25 @@
 let age = 22
 // console.log(typeof(age))
+
 let convert = String(age)
 // console.log(typeof(convert))
+
 let score = "100qq"// it will convert into number but it show NaN in console
 let convertion = Number(score)
-console.log(typeof(convertion))
-console.log(convertion)
+// console.log(typeof(convertion))
+// console.log(convertion)
+
+
 // let test = null
 let test = undefined
 let con = Number(test)
-console.log(typeof(con))
-console.log(con)
+// console.log(typeof(con))
+// console.log(con)
+
+
+//for boolean
+
+// let num = 1
+let num = 0
+let convertBool = Boolean(num)
+console.log(convertBool)
