b080d0841f0415436900439b7e03995cf88fa11e
Talal Azhar <talalazhar5656@gmail.com>
2026-07-08 21:03:08 +0500

    Stack vs Heap

--- conditions.js
+++ conditions.js
@@ -7,7 +7,27 @@
 //symbol
 let id = Symbol('12')
 let anotherId = Symbol('12')
-console.log(id == anotherId)
+// console.log(id == anotherId)
 
 let bignumber = 1223435252566634556666666n
-console.log(bignumber)
+// console.log(bignumber)
+
+//Stack vs Heap
+//Stack is for primitive data types
+//Heap is for Non primitive data types
+
+let name = "Talal"
+console.log(name)
+let anotherName = name
+anotherName = "Azhar"
+console.log(anotherName)
+
+let userId = {
+    email : "talal@gmail.com",
+    contact : 3140933024
+}
+console.log(userId.email)
+let secondUser = userId
+secondUser.email = "azhar@gmail.com"
+console.log(secondUser.email)
+console.log(userId.email)
