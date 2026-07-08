27c7c41b72145b6ca1b8837b82c1b4549b79ab82
Talal Azhar <talalazhar5656@gmail.com>
2026-07-08 17:55:51 +0500

    datatypes summary

--- conditions.js
+++ conditions.js
@@ -1,5 +1,13 @@
-console.log("2">2)
-console.log("02"==2)//convert 02 into number
-console.log("02"===2) //it also check datatype
-console.log(null==0)
-console.log(null>=0)//convert null into 0 then compare
+// console.log("2">2)
+// console.log("02"==2)//convert 02 into number
+// console.log("02"===2) //it also check datatype
+// console.log(null==0)
+// console.log(null>=0)//convert null into 0 then compare
+
+//symbol
+let id = Symbol('12')
+let anotherId = Symbol('12')
+console.log(id == anotherId)
+
+let bignumber = 1223435252566634556666666n
+console.log(bignumber)
