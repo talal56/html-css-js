5ba165a3fb27c9d107f37d6452461bd5f08a3299
Talal Azhar <talalazhar5656@gmail.com>
2026-07-08 16:38:21 +0500

    convertion

---
+++ conversion.js
@@ -0,0 +1,13 @@
+let age = 22
+// console.log(typeof(age))
+let convert = String(age)
+// console.log(typeof(convert))
+let score = "100qq"// it will convert into number but it show NaN in console
+let convertion = Number(score)
+console.log(typeof(convertion))
+console.log(convertion)
+// let test = null
+let test = undefined
+let con = Number(test)
+console.log(typeof(con))
+console.log(con)
